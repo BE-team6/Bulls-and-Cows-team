3c6d2088497c3bdde1fc872645ba7a05efa8b089
142spp <ldg142s@gmail.com>
2023-05-01 20:45:51 +0900

    feat : create main function
    Add main function with temp functios name

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import * from bnc_input
 import * from bnc_match
 import * from bnc_create
 
+if __name__ == '__main__':
+    answer = create_answer()
+    inp = bnc_input()
+    bNc = match(answer,inp)
+    print_result(bNc)
