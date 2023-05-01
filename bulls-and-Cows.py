e729f05aa499588f0bfca884d09b475b465ff5c5
jb <wjdqls6704@naver.com>
2023-05-01 20:31:28 +0900

    fix: Delete main
    Delete main for merge.
    Leave only the function part.

--- bulls-and-Cows.py
+++ bulls-and-Cows.py
@@ -3,7 +3,3 @@ import random
 def create_answer():
     num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     return random.sample(num, 4)
-
-if __name__ == "__main__":
-    answer = create_answer()
-    print(answer)
