ec5c7629acc095bd17cf6db34cb69b6fb7346fad
zigimi <chju0905@pusan.ac.kr>
2023-05-01 20:03:17 +0900

    feat: Print it's correct
    정답일때 it's correct 라는 문구를 출력

--- print.py
+++ print.py
@@ -1,6 +1,9 @@
 def print_result(bulls: int, cows: int):
-    print("bulls: "+ str(bulls))
-    print("cows: "+str(cows))
+    if bulls == 4:
+        print("it's correct")
+    else:
+        print("bulls: "+ str(bulls))
+        print("cows: "+str(cows))
 
 if __name__ == '__main__':
-    print_result(1, 2)
+    print_result(4, 4)
