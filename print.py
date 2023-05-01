c0e094da9c3aea998b42d4098d7e0e7991dc586b
zigimi <chju0905@pusan.ac.kr>
2023-05-01 19:56:41 +0900

    feat: Print Bulls
    bulls 값 출력

--- print.py
+++ print.py
@@ -1,5 +1,6 @@
-def print_result(bulls: int):
+def print_result(bulls: int, cows: int):
     print("bulls: "+ str(bulls))
+    print("cows: "+str(cows))
 
 if __name__ == '__main__':
-    print_result(1)
+    print_result(1, 2)
